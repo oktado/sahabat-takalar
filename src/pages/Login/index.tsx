60e69878e8b2e3a3625253d6ff13b855b9e48c18
Oktado Putra <oktado.putra@gmail.com>
2021-12-28 15:20:30 +0700

    create component login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+
+
+const Login = () => {
+  return (
+    <div className="container-login-page">
+        <div className='container-form-login' >
+            
+        </div>
+    </div>
+  );
+};
+
+export default Login;
